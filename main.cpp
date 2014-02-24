e914b6a916398cbdbcedaa38a9774b893b2275ee
Kevin Chapman <kchapmann@gmail.com>
2014-02-24 15:49:21 -0500

    creating 'tetrahedron_switch' branch

--- main.cpp
+++ main.cpp
@@ -13,29 +13,29 @@ glm::vec3 cubeTran;
 GLfloat size=10;
 
 GLfloat vertexarray[]={size,size,-size,
-		       	           size,-size,-size,
+		       size,-size,-size,
                        -size,-size,-size,
                        -size,size,-size,
                        size,size,size,
                        size,-size,size,
                        -size,-size,size,
                        -size,size,size
-                       };
+};
 
 GLfloat colorarray[]={1.0f,1.0f,1.0f,1.0f,
-	              			0.5f,1.0f,1.0f,1.0f,
-		      						1.0f,0.5f,1.0f,1.0f,
-		      						1.0f,1.0f,0.5f,1.0f,
-		      						1.0f,1.0f,1.0f,1.0f,
-		      						0.5f,1.0f,1.0f,1.0f,
-		      						1.0f,0.5f,1.0f,1.0f,
+		      0.5f,1.0f,1.0f,1.0f,
+		      1.0f,0.5f,1.0f,1.0f,
+		      1.0f,1.0f,0.5f,1.0f,
+		      1.0f,1.0f,1.0f,1.0f,
+		      0.5f,1.0f,1.0f,1.0f,
+		      1.0f,0.5f,1.0f,1.0f,
                       1.0f,1.0f,0.5f,1.0f
-	              			};
+};
 											
- GLubyte elems[]={0,1,2,3,7,4,5,6,
-    	          	7,3,0,4,5,6,2,1,
-    		  				0,1,5,4,7,3,2,6
-                 };
+GLubyte elems[]={0,1,2,3,7,4,5,6,
+		 7,3,0,4,5,6,2,1,
+		 0,1,5,4,7,3,2,6
+};
 
 void init(){
 	 glEnable(GL_DEPTH_TEST);
